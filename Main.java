a815239032e5528fd6c4660009d8adbdec0545e4
yeswanth-7 <yeswanthkilari64@gmail.com>
2025-09-13 23:26:53 +0530

    isodd in bit operation

--- Main.java
+++ Main.java
@@ -1,27 +1,16 @@
-import mysearch.*;
-
-public class Main {
+class Main{
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int target = 3;
-        int result = Recursivebs.Recursivebs(arr, target, arr.length - 1, 0);
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found");
+        //finding the if element is odd or even
+        int number = 29;
+
+        System.out.println("the number is " + (isodd(number)? " odd":" even"));
+
+    }
+    
+    static boolean isodd(int n) {
+        if ((n & 1) == 1) {
+            return true;
         }
-
-        int recursiveResult = Recursivebs.Recursivebs(arr, target, arr.length - 1,0);
-        if (recursiveResult != -1) {
-            System.out.println("Element found at index (recursive): " + recursiveResult);
-        } else {
-            System.out.println("Element not found (recursive)");
-        }
-
-        int lowerBound = Lowerb.lowerb(arr, target, arr.length);
-        System.out.println("Lower bound of " + target + " is: " + lowerBound);
-
-        int upperBound = Upperb.Upperb(arr, target, arr.length);
-        System.out.println("Upper bound of " + target + " is: " + upperBound);
+        return false;
     }
 }
